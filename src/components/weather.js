baa4d96500fbde9bff4ea76e838dc05b9d3530e8
H1ako <nikita-sobolev-wd@yandex.ru>
2021-09-15 16:16:56 +0400

    add styles and edit some functions

--- src/components/weather.js
+++ src/components/weather.js
@@ -5,16 +5,26 @@ import WeatherDay from './weather_day';
 class WeatherBoard extends React.Component {
 
     state = {
+        city_img: undefined,
         temp: undefined,
         city:  undefined,
         country: undefined,
         weather: undefined,
-        day: undefined
+        day: undefined,
+        error: undefined
     }
 
-    btn_on_click = async () => {
+    btn_on_click = () => {
         const city = prompt("City:");
-        this.getWeather(city);
+        if (city) this.getWeather(city)
+        else this.setState({error: 'Incorrect city'});        
+    }
+
+    getCityImg = async (city) => {
+        const url = `https://source.unsplash.com/featured/?${city} street`;
+        const data = await fetch(url);
+        console.log(data.url);
+        return data.url;
     }
 
     getWeekDate = () => {
@@ -24,30 +34,36 @@ class WeatherBoard extends React.Component {
     }
 
     getWeather = async (city='London') => {
-        const api_url = await 
+        const data = await 
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4976a831770c507ff01b1d19b4269107&units=metric`);
-        const data_json = await api_url.json();
-
-        this.setState({
-            temp: data_json.main.temp,
-            city: data_json.name,
-            country: data_json.sys.country,
-            weather: data_json.weather[0].main,
-            day: this.getWeekDate()
-        });
+        const data_json = await data.json();
+        if (data_json.cod !== '404') {
+            const img = await this.getCityImg(city);
+            this.setState({
+                city_img: img,
+                temp: data_json.main.temp,
+                city: data_json.name,
+                country: data_json.sys.country,
+                weather: data_json.weather[0].main,
+                day: this.getWeekDate(),
+                error: undefined
+            });
+        }
+        else{
+            this.setState({
+                error: 'Incorrect city'
+            });
+        }
     }
 
     componentDidMount() {
         this.getWeather();
-        console.log(this.getWeekDate());
     }
-
-    
 
     render() {
         return (
             <div className='weather-board'>
-                <div className="short-info">
+                <div className="short-info" style={{backgroundImage: `url(${this.state.city_img})`}}>
                     <div className="short-info__city">{this.state.city}, {this.state.country}</div>
                     <div className="short-info__temp">{this.state.temp}</div>
                     <div className="short-info__weather">{this.state.weather}</div>
@@ -57,8 +73,13 @@ class WeatherBoard extends React.Component {
                     temp={this.state.temp}
                     day={this.state.day}
                     weather={this.state.weather}
-                    country={this.state.country}/>
+                    country={this.state.country}
+                    />
                 </div>
+                { this.state.error &&
+                    <div id="error" class='error'>{this.state.error}</div>
+                }
+                
                 <button className="btn" onClick={this.btn_on_click}>Change Location</button>
             </div>
         );
